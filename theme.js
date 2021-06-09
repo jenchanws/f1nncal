a7b43597bcb8589659695fa4de56ded7ef7e7ffc
Maximilian Roll <maxroll54@gmail.com>
2021-06-09 05:50:53 +0200

    added browser theme detection and color adjustment

--- theme.js
+++ theme.js
@@ -1,3 +1,4 @@
+//Add transition class DOM and remove after 500ms
 let trans = () => {
     document.documentElement.classList.add('transition');
         window.setTimeout(() => {
@@ -5,9 +6,26 @@ let trans = () => {
         }, 500)
 }
 
-var isClicked = 0;
-var checkbox = document.querySelector('input[name=theme]')
-    checkbox.addEventListener('click', function(){
+var isClicked;
+
+//Detect if browser prefers dark or light mode
+//Set isClicked according to preference
+
+const isDarkTheme = window.matchMedia("(prefers-color-scheme: dark)")
+if (isDarkTheme.matches) {
+        document.documentElement.setAttribute('data-theme', 'dark')
+        document.getElementById("icon").src="icon_dark.png";
+        var isClicked = 1;
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light')
+        document.getElementById("icon").src="icon.png";
+        var isClicked = 0;
+}
+
+//Toggle theme and icon by clicking the icon
+
+var toggleImage = document.querySelector('input[name=theme]')
+    toggleImage.addEventListener('click', function(){
     if(isClicked == 0) {
         trans()
         document.documentElement.setAttribute('data-theme', 'dark')
