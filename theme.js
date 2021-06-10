3c413eba36097301d3655e01d67708c1d8216eff
Jennifer Chan <jenchanws@gmail.com>
2021-06-09 17:26:36 -0700

    Implement dark theme based on #3

---
+++ theme.js
@@ -0,0 +1,26 @@
+let browserPrefersDark = false
+
+const updateColorScheme = () => {
+  const shouldUseDarkTheme =
+    window.localStorage.getItem("theme") == "dark" || browserPrefersDark
+  document.body.className = shouldUseDarkTheme ? "theme-dark" : "theme-light"
+}
+
+const changeThemeOverride = () => {
+  if (!window.localStorage.getItem("theme")) {
+    window.localStorage.setItem("theme", browserPrefersDark ? "dark" : "light")
+  } else {
+    window.localStorage.setItem("theme",
+      window.localStorage.getItem("theme") == "dark" ? "light" : "dark")
+  }
+}
+
+window.matchMedia("(prefers-color-scheme: dark)").addListener(query => {
+  browserPrefersDark = query.matches
+  updateColorScheme()
+})
+
+document.getElementById("icon").addEventListener("click", () => {
+  changeThemeOverride()
+  updateColorScheme()
+})
