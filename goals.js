93e8213395b7e5ebda215636a6bb25c87f23379d
Jennifer Chan <jenchanws@gmail.com>
2021-08-09 22:26:02 -0700

    Add mechanism for showing past streams

--- goals.js
+++ goals.js
@@ -80,21 +80,3 @@ const goals = [
     type: "bobs",
   },
 ]
-
-const notes = [
-  {
-    date: [2021, 7, 7],
-    text:
-      "This break is extended to <b>July 14th</b> due to " +
-      "F1NN's nail and eyelash people being on holiday.<br>" +
-      "See <a href='http://redd.it/ogrkfo'>Instagram story</a>.",
-  },
-  {
-    date: [2021, 7, 15],
-    text:
-      "F1NN contracted SARS-CoV-2 and will not be able to " +
-      "do a Girl Month goal while recovering.<br>" +
-      "This break is extended <b>indefinitely</b>.<br>" +
-      "See <a href='https://www.twitch.tv/videos/1087703278'>Twitch stream</a>.",
-  }
-]
---
+++ notes.js
@@ -0,0 +1,9 @@
+const notes = [
+  {
+    date: [2021, 7, 7],
+    text:
+      "This break is extended to <b>July 14th</b> due to " +
+      "F1NN's nail and eyelash people being on holiday.<br>" +
+      "See <a href='http://redd.it/ogrkfo'>Instagram story</a>.",
+  },
+]
